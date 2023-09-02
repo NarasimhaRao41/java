77fa0db0696f3e2d96db28771712c723870dd3bf
NarasimhaRao41 <143867308+NarasimhaRao41@users.noreply.github.com>
2023-09-02 12:19:22 +0530

    Create WeekUsingSwitch.java

---
+++ WeekUsingSwitch.java
@@ -0,0 +1,38 @@
+//Java program to print days of week using Switch case
+//26/08/2023
+//WeekUsingSwitch.java
+import java.util.Scanner;
+public class Days {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int dayNum;
+    System.out.print("Enter a day number (1-7): ");
+    dayNum = in.nextInt();
+    switch(dayNum) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;       
+      default:
+        System.out.println("Invalid day number Please enter day number between (1-7).");
+        break;                      
+    } 
+  }
+}
