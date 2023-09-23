01dad192161387aaa7ef6c722d892f29b718aa90
NarasimhaRao41 <143867308+NarasimhaRao41@users.noreply.github.com>
2023-09-23 12:13:13 +0530

    Create NestedLoopPrime.java

---
+++ NestedLoopPrime.java
@@ -0,0 +1,26 @@
+#nested_loop_example or boolean_example
+#to_check whether the given number is prime or not
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int low = 23, high = 97;
+
+        while (low <= high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
